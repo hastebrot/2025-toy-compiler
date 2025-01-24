96f3c2e4c25806c67dbccb6aeadfc1691dbc305e
Benjamin Gudehus <hastebrot@gmail.com>
2025-01-24 20:27:54 +0100

    [NOSTORY] feat: Lezer grammar. (#1)

--- powerfx-js/index.ts
+++ powerfx-js/index.ts
@@ -1,1 +1,28 @@
-console.log("Hello via Bun!");
+import { type Tree } from "@lezer/common";
+import { buildParser, buildParserFile } from "@lezer/generator";
+
+function renderTree(tree: Tree) {
+  let items = "";
+  let level = 0;
+  tree.iterate({
+    enter({ type, from, to }) {
+      items += `${"".padEnd(level)}- ${type.name} (${from} → ${to})\n`;
+      level += 1;
+    },
+    leave() {
+      level -= 1;
+    },
+  });
+  return items;
+}
+
+if (import.meta.main) {
+  const grammar = await Bun.file("./sources/json.grammar").text();
+  const Parser = buildParser(grammar);
+  const parser = buildParserFile(grammar).parser;
+  console.log(parser);
+
+  const input = "[1, 2, 3, 4]";
+  const tree = Parser.parse(input);
+  console.log(renderTree(tree));
+}
