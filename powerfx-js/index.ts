75e868c2e083e88bfeff795bb9e8966623b2e1d2
Benjamin Gudehus <hastebrot@gmail.com>
2025-01-29 06:14:58 +0100

    [#2] feat: Expressions to syntax tree. (#3)

--- powerfx-js/index.ts
+++ powerfx-js/index.ts
@@ -2,27 +2,38 @@ import { type Tree } from "@lezer/common";
 import { buildParser, buildParserFile } from "@lezer/generator";
 
 function renderTree(tree: Tree) {
-  let items = "";
-  let level = 0;
+  let treeRepr = "";
   tree.iterate({
-    enter({ type, from, to }) {
-      items += `${"".padEnd(level)}- ${type.name} (${from} → ${to})\n`;
-      level += 1;
+    enter(nodeRef) {
+      const node = nodeRef.node;
+      const isNamedTerm = /^\w/.test(node.name);
+      treeRepr += isNamedTerm ? node.name : `"${node.name}"`;
+      if (node.firstChild !== null) {
+        treeRepr += "(";
+      } else if (node.nextSibling !== null) {
+        treeRepr += ",";
+      }
     },
-    leave() {
-      level -= 1;
+    leave(nodeRef) {
+      const node = nodeRef.node;
+      if (node.firstChild !== null) {
+        treeRepr += ")";
+        if (node.nextSibling !== null) {
+          treeRepr += ",";
+        }
+      }
     },
   });
-  return items;
+  return treeRepr;
 }
 
 if (import.meta.main) {
   const grammar = await Bun.file("./sources/json.grammar").text();
-  const Parser = buildParser(grammar);
-  const parser = buildParserFile(grammar).parser;
-  console.log(parser);
+  const Parser = buildParser(grammar, { includeNames: true });
+  const parserFile = buildParserFile(grammar, { includeNames: true });
+  console.log(parserFile.parser);
 
-  const input = "[1, 2, 3, 4]";
+  const input = "[1, 2, [3]]";
   const tree = Parser.parse(input);
   console.log(renderTree(tree));
 }
